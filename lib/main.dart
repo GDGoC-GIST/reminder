bbf19e18865975d4770b376ec66ad065cd518616
SSWAN1007 <roul0303@gmail.com>
2025-04-30 15:49:24 +0900

    notefortest

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+
+        //note for test!!
+
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
